7e7fff1e73533e8dd0f5a63a5faeeb851ea74518
scruze <shubham.khandelwal019@gmail.com>
2020-08-29 22:58:13 +0530

    added  login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import Home from './../components/Home';
-import Share from './../components/ShareWish';
+import LoginPage from './../login/LoginPage';
+import LandingPage from './../components/LandingPage'
 
 Vue.use(VueRouter)
 
   const routes = [
+    {
+      path: '/',
+      redirect: {
+          name: "login"
+      }
+  },
+    { path: '/login', name:'login', component: LoginPage },
+    { path: '/landingpage', name:'landing', component:  LandingPage},
+
   {
-    path: '/',
+    path: '/landingpage',
     name: 'Home',
     component: Home
   },
@@ -18,11 +28,6 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/share',
-    name: 'Share',
-    component: Share,
   }
 ]
 
